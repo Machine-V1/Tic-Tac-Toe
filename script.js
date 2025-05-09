f54ecf816a68b3be7536cced52761baf6b284e20
Ivan Ramos <ivan.ramos0909.22@gmail.com>
2025-05-09 00:38:33 -0300

    update

--- script.js
+++ script.js
@@ -8,31 +8,31 @@ Array.from(celulas).forEach((valor) => {
 });
 
 let ehbola = true;
-let vencedor = false;
+let vencedor = { venceu: false, quemVenceu: "" };
 let mapeamento = [
-  [],
-  [],
-  [],
+  ["vazio", "vazio", "vazio"],
+  ["vazio", "vazio", "vazio"],
+  ["vazio", "vazio", "vazio"],
 ];
 function preencherCelula(img, linha, coluna) {
   if (ehbola) {
     img.src = "circle_icon.svg";
     img.alt = "Bola";
     img.style.pointerEvents = "none";
-    console.log(linha, coluna);
+
     mapeamento[linha][coluna] = "O";
     ehbola = false;
   } else {
     img.src = "x_icon.svg";
     img.alt = "X";
     img.style.pointerEvents = "none";
-    console.log(linha, coluna);
+
     mapeamento[linha][coluna] = "X";
     ehbola = true;
   }
 }
 function clickCelula(divClicada) {
-  if (!divClicada.querySelector("img") && !vencedor) {
+  if (!divClicada.querySelector("img") && !vencedor.venceu) {
     let linha = parseInt(divClicada.dataset.linha) - 1;
     let coluna = parseInt(divClicada.dataset.coluna) - 1;
     const img = document.createElement("img");
@@ -40,15 +40,79 @@ function clickCelula(divClicada) {
     divClicada.appendChild(img);
     console.log(mapeamento);
     checarVencedor();
-  } else if (vencedor) {
-    window.alert("venceu!!");
   }
 }
-// let mapeamento = [
-//   ["x", "x", "x"],
-//   ["vazio", "vazio", "vazio"],
-//   ["vazio", "vazio", "vazio"],
-// ];
+
 function checarVencedor() {
   // desenvolver a logica obs: nao esquecer os metodos de arrays
+  let checkVencedor = null;
+  if (checkLinha()) {
+    checkVencedor = checkLinha();
+  } else if (checkColuna()) {
+    checkVencedor = checkColuna();
+  }
+  checkColuna();
+  console.log(checkVencedor);
+
+  if (checkVencedor == "O") {
+    vencedor.venceu = true;
+    vencedor.quemVenceu = "O";
+    window.alert(vencedor.quemVenceu + " venceu!!");
+  } else if (checkVencedor == "X") {
+    vencedor.venceu = true;
+    vencedor.quemVenceu = "X";
+
+    window.alert(vencedor.quemVenceu + " venceu!!");
+  }
+}
+
+function checkLinha() {
+  let checkLinhaString;
+  for (let linha of mapeamento) {
+    checkLinhaString = linha.reduce((total, valor) => (total += valor));
+
+    if (checkLinhaString === "OOO") {
+      return "O";
+    } else if (checkLinhaString === "XXX") {
+      return "X";
+    }
+  }
+  return null;
+}
+// let mapeamento = [
+//   ["O", "O", "X"],
+//   ["X", "X", "X"],
+//   ["O", "X", "O"],
+// ];
+function checkColuna() {
+  let checkColunaVetor = [];
+  let checkColunaString;
+  for (let i = 0; i < mapeamento.length; i++) {
+    for (linha of mapeamento) {
+      checkColunaVetor.push(linha[i]);
+    }
+
+    checkColunaString = checkColunaVetor.reduce(
+      (total, valor) => (total += valor)
+    );
+    if (checkColunaString === "OOO") {
+      return "O";
+    } else if (checkColunaString === "XXX") {
+      return "X";
+    }
+    checkColunaVetor = [];
+  }
+}
+
+function checkDiagonal() {
+  // 😥
+  let checkDiagonalVetor = [];
+  let checkDiagonalVetorString = "";
+  (function () {
+    for (let i = 0; i < 3; i++) {
+      // diagonal esquerda -> direita
+      vetor.push(mapeamento[i][i]);
+      // fazer aqui pra retornar o falor
+    }
+  })();
 }
--- teste.js
+++ teste.js
@@ -1,3 +1,24 @@
-let vetorx = ["a","a","b"]
-let vetory = ["a","a","b"]
-console.log(vetorx == vetory)
+// acessar o vetor na diagonal e armazenar em um vetor
+let mapeamento = [
+  ["a", "d", "g"],
+  ["b", "e", "h"],
+  ["c", "f", "i"],
+];
+let n = mapeamento.length;
+// mapeamento[linha][coluna]
+let vetor = [];
+
+for (let i = 0; i < 3; i++) {
+  // aqui acessa a linha
+  vetor.push(mapeamento[i][i]);
+}
+console.log(vetor);
+vetor = [];
+//
+
+for (let i = 0; i < 3; i++) {
+  // aqui acessa a linha
+  vetor.push(mapeamento[i][2-i]);
+  
+}
+console.log(vetor);
